150e5b24116fbedc4f6fda27c35787bfb4727922
Kinza-125 <ka8553200@gmail.com>
2024-10-21 08:25:27 -0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,14 +1,19 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer/Footer'
+import Hero from './components/Hero/Hero'
+import About from './about/page'
+import Portfolio from './portfolio/page'
 
-const About =()=>{
-    return(
-        <section id="about"className="py-16 bg-sky-700">
-<div className="container mx-auto text-center">
-<h2 className="text-4xl text-white font-bold mb-4">ABOUT ME</h2>
-<p className="text-lg text-white">I'm a web developer with a passion for creating
-    beautiful and functional websites.
-</p>
-</div>
-        </section>
-    )
+export default function Home(){
+  return(
+    <div>
+      
+        <Header></Header>
+        <Hero></Hero>
+        <About></About>
+        <Portfolio></Portfolio>
+        <Footer></Footer>
+    </div>
+  )
 }
-export default About
