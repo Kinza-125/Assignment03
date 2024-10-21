29aedfdae9e36004c4de8ae553a44b9114496e44
Kinza-125 <ka8553200@gmail.com>
2024-10-21 08:23:08 -0700

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    
+    <footer className="bg-teal-600 text-white p-6 mt-6">
+        <div className="max-w-5xl mx-auto text-center">
+          <p>&copy; 2024 My Awesome Website. All rights reserved.</p>
+        </div>
+      </footer>
+  
+  )
+}
+export default Footer
