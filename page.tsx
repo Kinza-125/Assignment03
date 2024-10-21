028e8ba45160153e414f5057ec3b8d580227257f
Kinza-125 <ka8553200@gmail.com>
2024-10-21 08:23:39 -0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,15 +1,14 @@
 
-
-
-const Portfolio = () => {
-    return (
-      <section id="portfolio" className="py-16 bg-gray-700">
-        <div className="container mx-auto text-center">
-          <h2 className="text-4xl text-white font-bold mb-4">My Projects</h2>
-          <p className="text-lg text-white">Here are some of my recent assignments and portfolio pieces.</p>
-        </div>
-      </section>
-    );
-  };
-  
-  export default Portfolio;
+const About =()=>{
+    return(
+        <section id="about"className="py-16 bg-sky-700">
+<div className="container mx-auto text-center">
+<h2 className="text-4xl text-white font-bold mb-4">ABOUT ME</h2>
+<p className="text-lg text-white">I'm a web developer with a passion for creating
+    beautiful and functional websites.
+</p>
+</div>
+        </section>
+    )
+}
+export default About
